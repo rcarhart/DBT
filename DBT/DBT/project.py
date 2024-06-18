c0a5ed01008ea9e90c6f9d524b868c12fa1b905b
rcarhart <rcarhart2@gmail.com>
2024-06-18 15:56:27 -0400

    deleted dbt folder

--- DBT/DBT/project.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from dagster_dbt import DbtProject
-
-DigitalData_project = DbtProject(
-    project_dir=Path(__file__).joinpath("..", "..", "..").resolve(),
-    packaged_project_dir=Path(__file__).joinpath("..", "..", "dbt-project").resolve(),
-)
-
